adcf3f1c3c35097cde0f2f70e84b0d43d5de8ac4
leandre <tuyambazeleandre@gmail.com>
2024-01-04 15:33:13 +0200

    (day-3): build dahsboard layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SideNav from '../ui/dashboard/sidenav';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
