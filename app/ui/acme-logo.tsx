44af2af37766148d5f772826c5c9c5945bfc695f
leandre <tuyambazeleandre@gmail.com>
2024-01-15 19:31:57 +0200

    Handling error while submitting form and change logo

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,4 +1,4 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { ArrowTrendingUpIcon } from '@heroicons/react/24/outline';
 import { lusitana } from './font';
 
 export default function AcmeLogo() {
@@ -6,8 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <ArrowTrendingUpIcon className="h-12 w-12 flex-shrink-0" />
+      <p className="flex-grow text-[40px] max-sm:text-center">iTrade</p>
     </div>
   );
 }
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -7,10 +7,10 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-20"
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 max-sm:justify-center md:h-20"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">
+        <div className="w-32 text-white max-sm:mr-10 md:w-40">
           <AcmeLogo />
         </div>
       </Link>
